9e2b0dc0abed7143e4efaef107f08ff641364e5b
Lenovo <d.shparko@mail.ru>
2022-11-27 18:00:24 +0300

    fead: add db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
@@ -17,11 +17,18 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
-    socket.on("join_room", () => {
-        socket.join(data);
-        console.log(`User with ID: ${socket.id} joined chat room.`);
+    socket.on("join_room", (data,sender,recipient) => {
+        socket.join(data[0]);
+        console.log(`sender: ${sender} recipient:${recipient} joined room: ${data[0]}`);
     });
 
+    socket.on("send_message", (data, topic) => {
+        socket.to(data.room).emit("receive_message", topic);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
 });
 
 server.listen(3001, () => {

c6840a490a9b664de25500f9e511674fd5dc86bf
Lenovo <d.shparko@mail.ru>
2022-11-29 11:26:04 +0300

    fix: fix code

--- index.js
+++ index.js
@@ -1,37 +1,79 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
-app.use(cors());
+const ws = require('ws')
 
-const server = http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
-});
-
-io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-
-    socket.on("join_room", (data,sender,recipient) => {
-        socket.join(data[0]);
-        console.log(`sender: ${sender} recipient:${recipient} joined room: ${data[0]}`);
-    });
-
-    socket.on("send_message", (data, topic) => {
-        socket.to(data.room).emit("receive_message", topic);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("User Disconnected", socket.id);
-    });
-});
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const routes = require('./routes/routes')
+const corsModdleware = require('./middleware/cors.middleware')
 
 
-server.listen(3001, () => {
-    console.log("SERVER RUNNING");
-});
+const app = express()
+const PORT = config.get('serverPort')
+
+app.use(corsModdleware)
+app.use(express.json())
+app.use('/api/auth', routes)
+
+const start = async () => {
+    try {
+        mongoose.connect(config.get('bdUrl'), {
+                useNewUrlParser:true,
+                useUnifiedTopology:true
+            })
+
+        
+
+
+        app.listen(PORT, () => {
+            console.log('Server is running on port', PORT);
+            console.log(`http://localhost:${PORT}`);
+        })
+    } catch (error) {
+        
+    }
+}
+
+start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////
+
+// const wss = new ws.Server({
+//     port: 5000,
+// }, () => console.log(`server started on 5000`))
+
+
+
+// wss.on('connection', function connection(ws) {
+//     ws.on('message', function (message) {
+//         message = JSON.parse(message)
+//         switch (message.event) {
+//             case 'message':
+//                 messageToAll(message)
+//                 break;
+//             case 'connenction':
+//                 messageToAll(message)
+//                 break
+// jasachatt6
+        
+//         }
+//     })
+// })
+
+// function messageToAll(message) {
+//     wss.clients.forEach(client => {
+//         client.send(JSON.stringify(message))
+//     })
+// }
+
